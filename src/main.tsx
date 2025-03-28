7b72edcffa956127da8d8a57f5f11b762def67fa
aurelien <rouchy.aurelien1gmail.com>
2025-03-28 12:47:48 +0100

    fix fireastore config

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { RouterProvider } from '@tanstack/react-router'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import { AuthProvider } from '@/lib/firebase/auth-context'
-import { router } from '@/routes'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "@tanstack/react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { AuthProvider } from "@/lib/firebase/auth-context";
+import { router } from "@/routes/index";
+import "./index.css";
 
 // Créer un client pour React Query
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 // Initialiser le routeur
-declare module '@tanstack/react-router' {
+declare module "@tanstack/react-router" {
   interface Register {
-    router: typeof router
+    router: typeof router;
   }
 }
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
@@ -25,5 +25,5 @@ createRoot(document.getElementById('root')!).render(
       </AuthProvider>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
